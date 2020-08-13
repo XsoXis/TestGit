54a0a53a316f39b34628d7ff14ec0f18a7454b2f
XsoXis <shin16266@hotmail.com>
2020-08-13 22:18:08 +0700

    Test
    Test

---
+++ method/method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your score is " + Math());
+            Console.ReadLine();
+        }
+        private static double Math ()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double z = x - y;
+            return z;
+        }
+    }
+}
